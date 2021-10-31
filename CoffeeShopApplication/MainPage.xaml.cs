0c8e6f8f7506d9d18a5345751cdca750659474f2
Campbell-Lythe-Brown <73807815+Campbell-Lythe-Brown@users.noreply.github.com>
2021-10-31 18:39:50 +1300

    Upload
    Upload

---
+++ CoffeeShopApplication/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CoffeeShopApplication
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void QuickBites_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new QuickBitesPage());
+        }
+
+        private async void ColdDrinks_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ColdDrinksPage());
+        }
+
+        private async void HotDrinks_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HotDrinksPage());
+        }
+
+        private async void Espresso_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EspressoPage());
+        }
+    }
+}
